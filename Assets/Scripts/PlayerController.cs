24766c77b6468ca940eedfa3ea8daf6db28bfef4
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-01-18 16:56:07 +0530

    RoomCreated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float lastmoveV;
+    float lastmoveH;
     public float walkSpeed;
     private Animator myAnim;
     private char facingDir;
@@ -18,13 +20,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float moveV = Input.GetAxis("Vertical");
         float moveH = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(moveH, moveV, 0.0f);
-        
-        myAnim.SetFloat("Horizontal", moveH);
-        myAnim.SetFloat("Vertical", moveV);
-        transform.position += movement * Time.deltaTime;
+        Vector3 movement = new Vector3(0, 0, 0);
+        if ((moveH!=0) && moveV == 0)
+        {
+            movement = new Vector3(moveH, moveV, 0.0f);
+
+            myAnim.SetFloat("Horizontal", moveH);
+            myAnim.SetFloat("Vertical", moveV);
+            transform.position += movement * Time.deltaTime;
+            lastmoveH = moveH;
+            lastmoveV = moveV;
+        }
+        else if ((moveV != 0) && moveH == 0)
+        {
+            movement = new Vector3(moveH, moveV, 0.0f);
+
+            myAnim.SetFloat("Horizontal", moveH);
+            myAnim.SetFloat("Vertical", moveV);
+            transform.position += movement * Time.deltaTime;
+            lastmoveV = moveV;
+            lastmoveH = moveH;
+        }
+        else if (moveV !=0 && moveH!=0)
+        {
+            movement = new Vector3(lastmoveH, lastmoveV, 0.0f);
+
+            myAnim.SetFloat("Horizontal", lastmoveH);
+            myAnim.SetFloat("Vertical", lastmoveV);
+            transform.position += movement * Time.deltaTime;
+
+        }
+        else
+        {
+            myAnim.SetFloat("Horizontal", 0);
+            myAnim.SetFloat("Vertical", 0);
+
+        }
+
+
     }
 
     void FixedUpdate()
